34acf2c8dc96662cadcf75befe0af238698c48c7
Eric Chi <ericjaychi@gmail.com>
2020-09-22 10:36:08 -0700

    Add comments

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# Import the package we installed in our Pipfile.
 import requests
 
+# Grabbing the JSON from a test API.
 response = requests.get("http://date.jsontest.com")
 
+# Print the date to confirm if it worked or not.
 print("Today's date is " + response.json()["date"])
